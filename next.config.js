5b8433e91082fc9f84a33d420e7c7c793a9909b0
Chun-Hui Gao <gaospecial@gmail.com>
2026-04-09 15:07:42 +0800

    build: add output: 'export' for static deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/v1/:path*',
-        destination: 'https://api.bio-spring.top/api/v1/:path*',
-      },
-    ]
-  },
+  output: 'export',
 }
 
 module.exports = nextConfig
